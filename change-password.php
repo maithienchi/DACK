ae4289a8b6f645b2f54d70ad128442c6dbbc3f06
hieu091099 <58078633+hieu091099@users.noreply.github.com>
2019-12-11 23:49:27 +0700

    Hieu Commit

--- change-password.php
+++ change-password.php
@@ -1,52 +1,67 @@
-<?php
-
-require 'init.php';
-if(!$currentUser){
-    header('Location: index.php');
-}
-//require 'functions.php';
-?>
-
-<?php
-    include 'header.php';
-?>
-<h1>Đổi mật khẩu</h1>
-<?php if(isset($_POST['currentPassword']) && isset($_POST['password'])): ?>
 <?php 
-$password = $_POST['password'];
-$currentPassword = $_POST['currentPassword'];
+  require_once 'init.php';
 
-$success = false;
+  // Xử lý logic ở đây
+  if(!$currentUser)
+  {
+    header('Location: index.php');
+    exit();
+  }
 
-
-if(password_verify($currentPassword,$currentUser['password'])&& ($password !=$currentPassword)) 
-{
-    UpdateUserPassword($currentUser['id'],$password);
-    $success =true;
-}
+ 
 
 ?>
-<?php if ($success): ?>
-<?php header('Location: index.php');?>
-<?php else : ?>
-<div class ="alert alert-danger" role="alert">Đổi mật khẩu thất bại</div>
-<?php endif; ?>
-<?php else : ?>
+<?php include 'header.php'; ?>
+<div clast="container" class="col-sm-4" >
+<h1>Đổi mật khẩu</h1>
+<?php if (isset($_POST['currentPassword']) && isset($_POST['password'])): ?>
+<?php
+    
+    $password=$_POST['password'];
+    $currentPassword=$_POST['currentPassword'];
+    
 
-<form action="change-password.php" method = "POST">
+    $succcess=false;
+    
+
+    if (password_verify($currentPassword,$currentUser['password']) ){
+        UpdateUserPassword($currentUser['id'],$password);
+      
+      $succcess=true;
+    }
+    
+?>
+<?php if ($succcess): ?>
+<div class="alert alert-primary" role="alert">
+<?php header('Location: index.php'); ?>
+</div>
+<?php else: ?>
+<div class="alert alert-danger" role="alert">
+    Đổi mật khẩu thất bại
+<form action="change-password.php">
+<button type="submit" class="btn btn-primary btn-lg">Đổi lại mật khẩu</button>
+</form>
+</div>
+<?php endif; ?>
+<?php else: ?>
+    <form action="change-password.php" method = "POST" >
+  
+
     <div class = "form-group">
-    <h2> <label for="currentPassword">Mật Khẩu hiện tại</label> </h2>
-    <input type="password" class = "form-control" id = "currentPassword" name = "currentPassword" placeholder = "Mật Khẩu hiện tại">
+    <label for="currentPassword">Mật khẩu hiện tại</label>
+    <input type="text" name="currentPassword" class="form-control" placeholder="Mật khẩu hiện tại">
     </div>
+
     <div class = "form-group">
-    <h2> <label for="password">Mật Khẩu mới</label> </h2>
-    <input type="password" class = "form-control" id = "password" name = "password" placeholder = "Mật khẩu mới ">
+    <label for="password">Mật khẩu Mới</label>
+    <input type="text" name="password" class="form-control" placeholder="Mật khẩu mới">
     </div>
 
     
-    <p><button type = "submit" class = "btn btn-primary">Đổi mật khẩu</button> </p>
+    <button type="submit" class="btn btn-primary btn-lg">Đổi mật khẩu</button>
 
     </form>
+    </div>
     
 <?php endif; ?>
 <?php include 'footer.php'; ?>
