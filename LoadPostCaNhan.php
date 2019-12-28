0c6ecd18527114300a2ff1465c1dde3c02b1ac4d
maithienchi <mtchi.17ck1@gmail.com>
2019-12-28 08:07:07 +0700

    sửa lỗi file xem tường nhà

--- LoadPostCaNhan.php
+++ LoadPostCaNhan.php
@@ -2,6 +2,43 @@
     require_once 'init.php';
     $PostNewCount = $_POST['PostNewCount'];
     $posts=getNewFeedsCaNhan($currentUser['id'],$PostNewCount);
+    $privacy="";
+  $privacy1="";
+  $privacy2="";
+  $image=null;
+  if(isset($_POST['content']))
+  {
+    if ( isset( $_FILES["image"] ) && !empty( $_FILES["image"]["name"] ) ) {
+      if ( is_uploaded_file( $_FILES["image"]["tmp_name"] ) && $_FILES["image"]["error"] == 0)
+      {
+        $fileName = $_FILES['image']['name'];
+        $fileSize = $_FILES['image']['size'];
+        $fileTemp = $_FILES['image']['tmp_name'];
+      // $filePath = './avatars/' . $currentUser['id'] . '.jpg';
+      //  move_uploaded_file($fileTemp, $filePath);
+        $newImage = resizeImage($fileTemp, 480, 480);
+        // imagejpeg($newImage, $filePath);
+        ob_start();
+        imagejpeg($newImage);
+        $image = ob_get_contents();
+        ob_end_clean();
+      }
+    }
+    if($image!=null){
+      createPost($currentUser['id'],$_POST['content'],$image,$_POST['privacy']);
+      }
+      header('Location: TrangCaNhan.php');
+  }
+  // if(isset($_POST['deletePost']))
+  // {
+  //   deletePostWithId($_POST['deletePost']);
+  //   header('Location:TrangCaNhan.php');
+  // } 
+  if(isset($_POST['privacy']) && $_POST['updatePost']!="")
+  {
+    UpdatePostById($_POST['updatePost'],$_POST['privacy']);
+    header('Location:TrangCaNhan.php');
+  }
 ?>
 <?php foreach ($posts as $post) : ?>
 <div class="card" style="margin-bottom: 10px;">
@@ -68,10 +105,95 @@
     <?php endif;?>
     <p class="card-text">
     <br>
-    <i class ='fa fa-thumbs-o-up' data-toggle="tooltip" title="Cảm xúc với status này!" style ='font-size:20px; color:black;'>&ensp;Thích</i>&emsp;
-    <i class ='far fa-comment-alt' data-toggle="tooltip" title="Viết bình luận" style ='font-size:20px; color:black;'>&ensp;Bình luận</i>&emsp;
-    <i class ='fa fa-share' data-toggle="tooltip" title="Gửi nội dung này đến bạn bè hoặc đăng trên dòng thời gian của bạn" style ='font-size:20px; color:black;'>&ensp;Chia sẻ</i>&emsp;
-   </p>
+
+
+    <?php
+      $countlike=DemLike($post['id']);
+      $countcommemnt=getcountcomments($post['id']);
+    ?>
+    <div class="card-body">
+    <hr>
+        <div class="row">
+            
+            <div class="btn-group" style="position: relative;bottom:6px;left:23px">
+            <?php if (KiemTraLike($post['id'],$currentUser['id'])): ?>
+            <a class="btn"name="unlike"id="unlike"href="likeCaNhan.php?type=unlike&id=<?php echo $post['id'] ?>"style='color:blue'><i style='font-size:20px' class='far fa-thumbs-up'data-toggle="tooltip" title="Cảm xúc của bạn với status này!!"></i> Thích  <span class="badge badge-primary  rounded-circle" ><?php echo implode(" ",$countlike);?></span></a>
+            <?php else:?>
+            <a class="btn"name="like"id="like"href="likeCaNhan.php?type=like&id=<?php echo $post['id'] ?>"style='color:black'><i style='font-size:20px' class='far fa-thumbs-up'data-toggle="tooltip" title="Cảm xúc của bạn với status này!!"></i> Thích  <span class="badge badge-light  rounded-circle"><?php echo implode(" ",$countlike);?></span></a>
+            <?php endif ?>
+            </div>&emsp;&emsp;
+            
+            <div>
+            <h9 aria-haspopup="true" aria-expanded="false"><i style='font-size:20px' class='far fa-comment-alt'data-toggle="tooltip" title="Cảm nghĩ của bạn về bài viết này!"></i> Bình luận<span class="badge badge-light  rounded-circle"><?php echo implode(" ",$countcommemnt);?></span></h9>
+            </div>&emsp;&emsp;
+            <div>
+                <h9 data-toggle="dropdown" aria-haspopup="true" aria-expanded="false"><i style="font-size:20px" class="fa fa-share" data-toggle="tooltip" title="Chia sẻ với bạn bè" aria-hidden="true"></i><span class="sr-only">Chia sẻ với bạn bè</span> Chia sẻ </h9>
+                <div class="dropdown-menu">
+                    <a class="dropdown-item" href="#">Chia sẻ ngay (Công khai)</a>
+                    <a class="dropdown-item" href="#">Chia sẻ ...</a>
+                    <a class="dropdown-item" href="#">Gửi dưới dạng tin nhắn</a>
+                    <a class="dropdown-item" href="#">Chia sẻ trên dòng thời gian với bạn bè</a>
+                    <a class="dropdown-item" href="#">Chia sẻ lên trang</a>
+                </div>
+            </div>
+        </div>
+    <hr>
+    <?php $getcomment=getcomment($post['id']);
+    ?>
+    <?php if(usercommentd($post['id'],$currentUser['id'])): ?>
+    <div class="target" style="height:200px;overflow:scroll;" >
+        <?php foreach ($getcomment as $postss):?>
+            
+        <div class="card" >
+        <div class="card-body">
+                <h5 class="card-title">
+                    <?php if($postss['avatar']):?> 
+                      <img src ="avatar.php?id=<?php echo $postss['userId']; ?>" class="img-circle" alt="Cinque Terre" width="35" height="35">  
+                    <?php else: ?>
+                    <img src="no-avatar.jpg" class="img-circle" alt="Cinque Terre" width="35" height="35">
+                    <?php endif ?> 
+                    <a href="profile.php?id=<?php echo $postss['userId'] ?>"><div style="position: absolute; left:80px;top:20px " ><?php echo $postss['displayName']?> </div> </a>          
+                  </h5> 
+
+                  <small><p class="card-text"style="position: absolute; left:80px;top:50px" > Bình luận lúc: 
+                    <?php echo $postss['createdAt'];?>
+                </p></small>
+                <p >
+                    <?php echo $postss['content'];?>
+                </p>
+                <div class="col"style="text-align: right;position: absolute; left:8px;top:8px ">
+                <form method="post">
+                <button  type="submit" name="deletecomment" value = <?php echo $postss['id']; ?>  class="btn btn-danger" >Xóa</button>   
+                </form>
+
+                <?php 
+                if(isset($_POST['deletecomment']))
+                {
+                    $value_commnet=$_POST['deletecomment'];
+                    
+                    deletecomment($value_commnet);
+                    header("Location: index.php");
+                    }
+                ?>
+                  
+            </div>  
+            </div>  
+            </div>
+        <?php endforeach ?>
+                        </div>
+    <?php else:?>
+            <div></div>
+    <?php endif?>
+    <form action="upcomment1.php?type=upcommentindex&id=<?php echo $post['id'] ?>" method="POST" >
+    <div class="form-group">
+        <textarea style="height:50px" class="form-control" name="contents" id="contents" rows="3"placeholder="Thêm bình luận..."></textarea>                                
+    </div>        
+    <button type="submit" class="btn btn-primary" name="upcomment">comment</button>
+    </form>	
+</div>
+   
+  
+  </p>
   </div>
 </div>
 <?php endforeach; ?>
--- LoadPostWall.php
+++ LoadPostWall.php
@@ -1,144 +1,39 @@
-<?php
-require_once 'init.php';
-$posts=getNewFeedsCaNhan($_GET['id'],10);
-$profile = findUserById($_GET['id']);
-$friends = getFriends($currentUser['id']);
-$isFriend = false;
-foreach ($friends AS $friend) {
-  if ($friend['id'] == $profile['id']) {
-    $isFriend = true;
-  }
-}
-$isFollow = isFollow($currentUser['id'], $profile['id']);
-$isRequest = isFollow($profile['id'], $currentUser['id']);
-$KiemTraFollow=KiemTraFollow($currentUser['id'], $profile['id']);
+<?php 
+    require_once 'init.php';
+    $PostNewCount = $_POST['PostNewCount'];
+    $posts=getNewFeedsCaNhan($_GET['id'],$PostNewCount);
+    $profile = findUserById($_GET['id']);
+    $friends = getFriends($currentUser['id']);
+    $isFriend = false;
+    foreach ($friends AS $friend) {
+    if ($friend['id'] == $profile['id']) {
+        $isFriend = true;
+    }
+    }
+    $isFollow = isFollow($currentUser['id'], $profile['id']);
+    $isRequest = isFollow($profile['id'], $currentUser['id']);
+    $KiemTraFollow=KiemTraFollow($currentUser['id'], $profile['id']);
 
 
-$TheoDoi="";
-$MauTheoDoi = "";
+    $TheoDoi="";
+    $MauTheoDoi = "";
 
-if (isset($_POST['NhanTin']))
-{
-  header('Location: conversation.php?id=' . $_GET['id']);
-}
-if(isset($_POST['TheoDoi']))
-{
-  if($KiemTraFollow){
-    HuyFollow($currentUser['id'], $profile['id']);
-  }
-  else {
-    Follow($currentUser['id'], $profile['id']);
-  }
-  header('Location: wall.php?id=' . $_GET['id']);
-}
+    if (isset($_POST['NhanTin']))
+    {
+    header('Location: conversation.php?id=' . $_GET['id']);
+    }
+    if(isset($_POST['TheoDoi']))
+    {
+    if($KiemTraFollow){
+        HuyFollow($currentUser['id'], $profile['id']);
+    }
+    else {
+        Follow($currentUser['id'], $profile['id']);
+    }
+    header('Location: wall.php?id=' . $_GET['id']);
+    }
+
 ?>
-<?php include 'header.php' ?>
-<h1>Tường nhà <?php echo $profile['displayName'] ?></h1>
-<?php if($profile['avatar']): ?>
-    <img src ="avatar.php?id=<?php echo $profile['id']; ?>"  alt="Cinque Terre" >
-    <?php else: ?>
-    <img src="no-avatar.jpg"  alt="Cinque Terre" >
-    <?php endif;?>
-    <h3>Thông Tin Cá Nhân</h3>
-<li>
-    <b>Họ Tên: </b><?php echo $profile['displayName']; ?><br>
-</li>
-<?php if($isFriend):?>
-  <?php if($profile['privacyEmail']==('Công khai' or 'Bạn bè')):?>
-<li>
-    <b>Email: </b><?php echo $profile['email']; ?>
-</li>
-  <?php endif;?>
-<?php elseif($profile['privacyEmail']=='Công khai'): ?>
-  <li><b>Email: </b><?php echo $profile['email']; ?></li>
-<?php endif;?>
-
-<?php if($isFriend):?>
-  <?php if($profile['privacyGioiTinh']==('Công khai' or 'Bạn bè')):?>
-<li>
-    <b>Giới tính: </b><?php echo $profile['gender']; ?>
-</li>
-<?php endif;?>
-<?php elseif($profile['privacyGioiTinh']=='Công khai'): ?>
-  <li><b>Giới tính: </b><?php echo $profile['gender']; ?></li>
-  <?php endif;?>
-
-  <?php if($isFriend):?>
-  <?php if($profile['privacyNamSinh']==('Công khai' or 'Bạn bè')):?>
-<li>
-    <b>Ngày Sinh: </b><?php echo $profile['NamSinh']; ?>
-</li>
-<?php endif;?>
-<?php elseif($profile['privacyNamSinh']=='Công khai'): ?>
-  <li><b>Ngày Sinh: </b><?php echo $profile['NamSinh']; ?></li>
-<?php endif;?>
-
-
-<?php if($isFriend):?>
-  <?php if($profile['privacyPhone']==('Công khai' or 'Bạn bè')):?>
-<li>
-    <b>Số điện thoại: </b><?php echo $profile['phone']; ?>
-</li>
-<?php endif;?>
-<?php elseif($profile['privacyPhone']=='Công khai'): ?>
-  <li><b>Số điện thoại: </b><?php echo $profile['phone']; ?></li>
-<?php endif;?>
-
-<?php if($currentUser['id']!=$_GET['id']): ?>
-<div class="row">
-  <div class="col-sm-0">
-<?php if ($isFriend) : ?>
-<form action="friend-request.php" method="POST">
-  <input type="hidden" name="action" value="unfriend">
-  <input type="hidden" name="profileId" value="<?php echo $profile['id'] ?>">
-  <button type="submit" class="btn btn-danger">Hủy kết bạn</button>
-</form>
-<?php else : ?>
-  <?php if ($isFollow) : ?>
-    <form action="friend-request.php" method="POST">
-      <input type="hidden" name="action" value="cancel-friend-request">
-      <input type="hidden" name="profileId" value="<?php echo $profile['id'] ?>">
-      <button type="submit" class="btn btn-primary">Hủy yêu cầu kết bạn</button>
-    </form>
-  <?php elseif ($isRequest) : ?>
-    <form action="friend-request.php" method="POST">
-      <input type="hidden" name="action" value="accept-friend-request">
-      <input type="hidden" name="profileId" value="<?php echo $profile['id'] ?>">
-      <button type="submit" class="btn btn-primary">Chấp nhận yêu cầu kết bạn</button>
-    </form>
-    <form action="friend-request.php" method="POST">
-      <input type="hidden" name="action" value="reject-friend-request">
-      <input type="hidden" name="profileId" value="<?php echo $profile['id'] ?>">
-      <button type="submit" class="btn btn-warning">Từ chối yêu cầu kết bạn</button>
-    </form>
-  <?php else : ?>
-    <form action="friend-request.php" method="POST">
-      <input type="hidden" name="action" value="send-friend-request">
-      <input type="hidden" name="profileId" value="<?php echo $profile['id'] ?>">
-      <button type="submit" class="btn btn-primary">Gửi yêu cầu kết bạn</button>
-    </form>
-  <?php endif; ?>
-<?php endif; ?>
-</div>
-
-  <?php if($KiemTraFollow):?>
-  <?php $TheoDoi="Đang theo dõi";$MauTheoDoi='class="btn btn-success"'?>
-  <?php else: ?>
-    <?php $TheoDoi="Theo dõi";$MauTheoDoi='class="btn btn-warning"'?>
-  <?php endif;?>
-&nbsp;
-<form method="POST">
-  <button type="submit" <?php echo $MauTheoDoi; ?> name="TheoDoi"><?php echo $TheoDoi; ?></button>
-</form>
-&nbsp;
-<form method="POST">
-  <button type="submit" class="btn btn-primary" name="NhanTin">Nhắn Tin</button>
-</form>
-</div>
-
-  <?php endif; ?>
-<!-- Xuất Bài viết -->
-<div id="posts">
 <?php foreach ($posts as $post) : ?>
 <?php if ($isFriend): ?> 
   <?php if($post['privacy']=='Công khai'|| $post['privacy']=='Bạn bè' ): ?>  
@@ -381,29 +276,3 @@ if(isset($_POST['TheoDoi']))
   <?php endif; ?>
 <?php endif; ?>
 <?php endforeach; ?>
-</div>
-
-<br>
-<br>
-<br>
-<br>
-<br>
-<br>
-<br>
-<!-- <button id="btnLoadPost">Xem Thêm</button> -->
-<div class="card" style="margin-bottom: 10px;">
-    <button id="btnLoadPost">Tải thêm</button>
-  </div>
-<?php include 'footer.php' ?>
-<script>
-        //jQuery code here!
-        $(document).ready(function(){
-        var PostCount = 10;
-        $('#btnLoadPost').click(function(){
-            PostCount=PostCount+10;
-            $('#posts').load("LoadPostWall.php?id=<?php echo $_GET['id']; ?>",{
-            PostNewCount: PostCount
-            });
-        });
-        });
-</script>
